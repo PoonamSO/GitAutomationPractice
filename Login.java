a81f2c8e515c570227cede20a988d244f27f93de
PoonamSO <poonamsonawane603@gmail.com>
2022-11-17 12:01:16 +0530

    First commmit

---
+++ Login.java
@@ -0,0 +1,11 @@
+public static Login{
+    public static login(String str, int pswd){
+	{
+	     System.out.println("Username is"+str+"Password is "+pswd);
+	}
+    public static void main(String[] args){
+	     login("ABC",123);
+	}
+
+
+}	
